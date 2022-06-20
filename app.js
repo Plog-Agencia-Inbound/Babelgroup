ab2e3a779d1361ece6c1a45694c40d2b86a79b66
Roberto Aceves <tecnologia@plog.mx>
2022-06-20 11:58:22 -0500

    Menu Avanzado Movil

--- app.js
+++ app.js
@@ -23,4 +23,38 @@ function closeMenu() {
         sidemenu.classList.add("menu-collapsed");
     }
     //sidemenu.getAttribute("class") == "menu-expanded" ? sidemenu.classList.remove("menu-expanded") : sidemenu.classList.add("menu-collapsed");  
-  }
+}
+
+// Funciones de Busqueda (Rango)
+
+/* Rango de precio */
+var output = document.getElementById("output");
+var range = document.getElementById("range_slider");
+output.innerHTML = numeral(range.value).format('$0,0.00');
+range.oninput = function() {
+    output.innerHTML = numeral(range.value).format('$0,0.00');
+}
+
+/* Rango de area */
+var output_area = document.getElementById("area_output");
+var range_area = document.getElementById("area_slider");
+output_area.innerHTML = numeral(range_area.value).format('0,0')+' Mts2';
+range_area.oninput = function() {
+    output_area.innerHTML = numeral(range_area.value).format('0,0')+' Mts2';
+}
+
+// Funciones de Busqueda (Busqueda Avanzada)
+
+const btn_bA = document.getElementById("btn_bA");
+const bAswitch = document.getElementById("bAswitch");
+const bAContent = document.getElementById("bAContent");
+
+btn_bA.addEventListener('click', function(){
+    if(bAswitch.getAttribute("class") == "bA_on"){
+        bAswitch.classList.remove("bA_on");
+        bAswitch.classList.add("bA_off");
+
+        bAContent.classList.remove("bA_off");
+        bAContent.classList.add("bA_on");
+    }
+});
