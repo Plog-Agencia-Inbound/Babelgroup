7fb7aa3a199b0dfa308922f5fafe0d50204dcec7
Roberto Aceves <tecnologia@plog.mx>
2022-06-20 10:04:07 -0500

    Funciones Menu
    Se añadieron las funciones de menu en Javascript

--- app.js
+++ app.js
@@ -1,90 +1,26 @@
+// Funciones de Menu
 
-/* Modulo Menu*/
+var openmenu = document.querySelector(".openMenu");
+var closemenu = document.querySelector(".closeMenu");
 
-const menuContent = document.querySelector("#menu_content");
-const menuSearch = document.querySelector("#menu_search");
+var sidemenu = document.querySelector("#sidemenu");
 
-const btn_close = document.querySelector('#menu_close');
-const btn_open = document.querySelector('#menu_open');
-const menu = document.querySelector('#navigation_menu');
+openmenu.addEventListener('click', openMenu);
+closemenu.addEventListener('click', closeMenu);
 
-const busquedaAvanzada = document.querySelector('#search_filter'); 
-
-busquedaAvanzada.addEventListener('click',openSearch);
-
-btn_close.addEventListener('click', closeMenu);
-btn_open.addEventListener('click', openMenu);
-
-function openMenu(){
-    if(menuContent.getAttribute("class")=="off_select"){
-        menuContent.classList.remove("off_select");
-        menuContent.classList.add("on_select")
-        menuSearch.classList.remove("on_select");
-        menuSearch.classList.add("off_select");
+function openMenu() {
+    if(sidemenu.getAttribute("class") == "menu-collapsed"){
+        sidemenu.classList.remove("menu-collapsed");
+        sidemenu.classList.add("menu-expanded");
     }
-    
-    
-    menu.classList.remove("menu-collapsed");
-    menu.classList.add("menu-expanded");
+
+  //sidemenu.getAttribute("class") == "menu-collapsed" ? sidemenu.classList.remove("menu-collapsed") : sidemenu.classList.add("menu-expanded");  
 }
 
-function closeMenu(){
-    menu.classList.remove("menu-expanded");
-    menu.classList.add("menu-collapsed");
-}
-
-function openSearch(){
-    if(menuSearch.getAttribute("class")=="off_select"){
-        menuSearch.classList.remove("off_select");
-        menuSearch.classList.add("on_select")
-        menuContent.classList.remove("on_select");
-        menuContent.classList.add("off_select");
+function closeMenu() {
+    if(sidemenu.getAttribute("class") == "menu-expanded"){
+        sidemenu.classList.remove("menu-expanded");
+        sidemenu.classList.add("menu-collapsed");
     }
-    
-    
-    menu.classList.remove("menu-collapsed");
-    menu.classList.add("menu-expanded");
-}
-
-
-
-
-/* Switch */
-const btnCompra = document.querySelector("#compra");
-const btnRenta = document.querySelector("#renta");
-var inputSwith = document.querySelector("#switch");
-
-btnCompra.addEventListener('click', e => {
-    
-    inputSwith.value = "compra";
-    if (btnCompra.getAttribute("class") == "check") {
-        console.log(btnCompra.getAttribute("class"));
-    }
-    else {
-        btnCompra.classList.remove("no-check");
-        btnCompra.classList.add("check")
-        btnRenta.classList.remove("check");
-        btnRenta.classList.add("no-check");
-    }
-});
-
-btnRenta.addEventListener('click', e => {
-    inputSwith.value = "Renta";
-    if (btnRenta.getAttribute("class") == "check") {
-        console.log(btnRenta.getAttribute("class"));
-    }
-    else {
-        btnRenta.classList.remove("no-check");
-        btnRenta.classList.add("check")
-
-        btnCompra.classList.remove("check");
-        btnCompra.classList.add("no-check");
-    }
-});
-
-/* Rango de superficie */
-var superficie = document.querySelector("#in_superficie");
-var pointer = document.querySelector("#out_superfice");
-pointer.oninput = function() {
-    superficie.innerHTML = this.value;
-}
+    //sidemenu.getAttribute("class") == "menu-expanded" ? sidemenu.classList.remove("menu-expanded") : sidemenu.classList.add("menu-collapsed");  
+  }
